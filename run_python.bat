1818c953eb0ef18efe92e04fb3d8998a1e9c31c1
Chris <chris@seniorcodereviewbuddy.com>
2024-04-29 11:48:36 -0400

    Trigger pytest through python instead of directly in Sublime (#6)
    This means Sublime doesn't need to worry about the path to pytest,
    it will let python figure it out.
    
    Also remove remove batch files comments that were incorrectly landed.

--- run_python.bat
+++ run_python.bat
@@ -14,22 +14,10 @@ REM must be 0 and we treat it as a test file and run it with pytest.
 REM See the following stackoverflow page for more details:
 REM https://stackoverflow.com/questions/7005951/batch-file-find-if-substring-is-in-string-not-in-a-file
 
-
-::set file_path=%1
-::set file_path_without_test_py=%file_path:_test.py=%
-
-::IF %file_path% == %file_path_without_test_py% (
-::%DELIBERATE_PRACTICE_CONDA_DIR%\python.exe %1
-::) ELSE (
-::%DELIBERATE_PRACTICE_CONDA_DIR%\Scripts\pytest.exe %1
-::) 
-
-::set file_path=%1
-::set file_path_without_test_py=%file_path:_test.py=%
 echo:%1| findstr /c:"_test.py" /i /e 1>nul
 
 IF ERRORLEVEL 1 (
 %DELIBERATE_PRACTICE_CONDA_DIR%\python.exe %1
 ) ELSE (
-%DELIBERATE_PRACTICE_CONDA_DIR%\Scripts\pytest.exe %1
+%DELIBERATE_PRACTICE_CONDA_DIR%\python.exe -m pytest %1
 ) 
