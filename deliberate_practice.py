55bb0d3cecdfcfac5f4f05d32e059b2ff5398fda
Chris <chris@seniorcodereviewbuddy.com>
2024-05-01 17:09:21 -0400

    Create the initial CLI flow for users to pick the run mode (#5)
    Currently both run modes just print their name and terminate,
    funcationality to be added later.

--- deliberate_practice.py
+++ deliberate_practice.py
@@ -1,6 +1,43 @@
 """Main entry point for the Deliberate Practice CLI."""
 
+import enum
 
-def main() -> None:
+import user_input
+
+
+class InvalidModeError(Exception):
+    """An invalid mode was selected."""
+
+
+class RunMode(enum.StrEnum):
+    """The valid run modes of the deliberate practice app."""
+
+    PRACTICE = "Practice"
+    EVALUATION = "Evaluation"
+
+
+def select_run_mode(fetch_input: user_input.FetchInputWithPrompt) -> RunMode:
+    """Prompts the user to determine what RunMode to launch."""
+    prompt = (
+        "Welcome to the Deliberate Practice CLI" + "\n"
+        "Which mode do you wish to run in?"
+    )
+    all_modes = list(RunMode)
+
+    pick = user_input.prompt_for_choice(fetch_input, prompt, all_modes)
+    return RunMode(pick)
+
+
+def main(fetch_input: user_input.FetchInputWithPrompt) -> None:
     """Starts the Deliberate Practice CLI."""
-    print("Running Deliberate Practice Main")
+    selected_run_mode = select_run_mode(fetch_input)
+    if selected_run_mode == RunMode.PRACTICE:
+        print("Practice Mode Selected")
+    elif selected_run_mode == RunMode.EVALUATION:
+        print("Evaluation Mode Selected")
+    else:
+        raise InvalidModeError(f"Unexpected mode given, {selected_run_mode}")
+
+
+if __name__ == "__main__":
+    main(input)
---
+++ test_utils/mock_input.py
@@ -0,0 +1,20 @@
+"""A module containing MockInput class for tests."""
+
+
+class MockInput:  # pylint: disable=too-few-public-methods
+    """A MockInput class that can replace input for tests."""
+
+    def __init__(self, inputs: list[str]):
+        """Initialize the instance with inputs to return when called."""
+        self.inputs = inputs
+
+    def __call__(self, _prompt: str) -> str:
+        """Returns the next input string.
+
+        Raises:
+            EOFError: if there is no more input, similar to input().
+        """
+        if not self.inputs:
+            raise EOFError
+
+        return self.inputs.pop(0)
