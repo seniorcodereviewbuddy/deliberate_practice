032b031eeba6c375a1eb44137a2e8eeec58d976c
Chris <chris@seniorcodereviewbuddy.com>
2024-05-01 19:39:51 -0400

    Change input_mock module to mocks module (#6)
    This is a shorter name and makes it a bit clearer what else could be in
    that module.

--- deliberate_practice_test.py
+++ deliberate_practice_test.py
@@ -2,7 +2,7 @@ import pytest
 
 import deliberate_practice
 import user_input
-from test_utils import mock_input
+from test_utils import mocks
 
 
 @pytest.mark.parametrize(
@@ -15,12 +15,12 @@ from test_utils import mock_input
 def test_select_run_mode_choice_made(
     user_selection: str, expected_mode: deliberate_practice.RunMode
 ) -> None:
-    input_mock = mock_input.MockInput([str(user_selection)])
+    input_mock = mocks.MockInput([str(user_selection)])
     selected_mode = deliberate_practice.select_run_mode(input_mock)
     assert selected_mode == expected_mode
 
 
 def test_select_run_mode_no_choice() -> None:
-    input_mock = mock_input.MockInput([])
+    input_mock = mocks.MockInput([])
     with pytest.raises(user_input.NoChoiceMadeError):
         deliberate_practice.select_run_mode(input_mock)
--- user_input_test.py
+++ user_input_test.py
@@ -1,7 +1,7 @@
 import pytest
 
 import user_input
-from test_utils import mock_input
+from test_utils import mocks
 
 
 class TestPromptUserForChoice:
@@ -10,42 +10,42 @@ class TestPromptUserForChoice:
     @pytest.mark.parametrize(("pick", "expected_value"), [(1, "a"), (2, "b"), (3, "c")])
     def test_valid_choices(self, pick: int, expected_value: str) -> None:
         choices = ["a", "b", "c"]
-        input_mock = mock_input.MockInput([str(pick)])
+        input_mock = mocks.MockInput([str(pick)])
         selection = user_input.prompt_for_choice(input_mock, self.basic_prompt, choices)
         assert selection == expected_value
 
     def test_valid_choice_only_one_option(self) -> None:
         choices = ["a"]
-        input_mock = mock_input.MockInput(["1"])
+        input_mock = mocks.MockInput(["1"])
         selection = user_input.prompt_for_choice(input_mock, self.basic_prompt, choices)
         assert selection == "a"
 
     def test_valid_choice_after_invalid_str(self) -> None:
-        input_mock = mock_input.MockInput(["invalid_choice", "1"])
+        input_mock = mocks.MockInput(["invalid_choice", "1"])
         choices = ["a", "b", "c"]
         selection = user_input.prompt_for_choice(input_mock, self.basic_prompt, choices)
         assert selection == "a"
 
     def test_valid_choice_after_invalid_int(self) -> None:
-        input_mock = mock_input.MockInput(["7", "1"])
+        input_mock = mocks.MockInput(["7", "1"])
         choices = ["a", "b", "c"]
         selection = user_input.prompt_for_choice(input_mock, self.basic_prompt, choices)
         assert selection == "a"
 
     def test_valid_choice_after_negative_int(self) -> None:
-        input_mock = mock_input.MockInput(["-7", "2"])
+        input_mock = mocks.MockInput(["-7", "2"])
         choices = ["a", "b", "c"]
         selection = user_input.prompt_for_choice(input_mock, self.basic_prompt, choices)
         assert selection == "b"
 
     def test_no_choice_made(self) -> None:
-        input_mock = mock_input.MockInput([])
+        input_mock = mocks.MockInput([])
         choices = ["a", "b", "c"]
         with pytest.raises(user_input.NoChoiceMadeError):
             user_input.prompt_for_choice(input_mock, self.basic_prompt, choices)
 
     def test_no_choice_made_after_invalid_choice(self) -> None:
-        input_mock = mock_input.MockInput(["5"])
+        input_mock = mocks.MockInput(["5"])
         choices = ["a", "b", "c"]
         with pytest.raises(user_input.NoChoiceMadeError):
             user_input.prompt_for_choice(input_mock, self.basic_prompt, choices)
