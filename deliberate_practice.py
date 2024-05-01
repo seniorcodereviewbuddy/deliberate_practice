ea1ac93a99dd3818026b881f827f677eef00ad82
Chris Sharp <chris@seniorcodereviewbuddy.com>
2024-05-01 15:03:46 -0400

    Add Ruff and enable pydocstyle linting
    Disable multiple docstring checks in flake8 since only ruff should look at
    those.

--- deliberate_practice.py
+++ deliberate_practice.py
@@ -1,6 +1,6 @@
+"""Main entry point for the Deliberate Practice CLI."""
+
+
 def main() -> None:
+    """Starts the Deliberate Practice CLI."""
     print("Running Deliberate Practice Main")
-
-
-if __name__ == "__main__":
-    main()
