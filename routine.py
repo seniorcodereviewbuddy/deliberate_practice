773a6d349c0295bb6260aa50f1a7dd748a908a6a
Chris <chris@seniorcodereviewbuddy.com>
2024-05-02 16:08:35 -0400

    Land initial Activity and Activities classes (#8)
    These are the bases of how users will create practie routines.
    
    Usage to follow in a later PR.

---
+++ routine.py
@@ -0,0 +1,81 @@
+"""Collection of the classes related to a practice routine."""
+
+import pathlib
+import random
+
+
+class InvalidActivitiesFileError(Exception):
+    """There was an issue with the activities file."""
+
+
+class Activities:
+    """Activities is the root of all practice routine information.
+
+    Activities is responsible for managing all the practice activities,
+    including deciding what activity should be done next.
+    """
+
+    def __init__(self, activity_file: pathlib.Path):
+        """Creates an Activities instance from the specified file."""
+        self.activities = []
+        try:
+            with open(activity_file, encoding="utf-8") as f:
+                for line in f.readlines():
+                    self.activities.append(Activity(line.strip()))
+        except FileNotFoundError as e:
+            raise InvalidActivitiesFileError(
+                "No Activity file found, please create an activity file. "
+            ) from e
+
+        if not self.activities:
+            raise InvalidActivitiesFileError(
+                "No activities found in activity file. Please add activities."
+            )
+
+    def get_activity_descriptions(self) -> list[str]:
+        """Returns all the Activity descriptions, in key order."""
+        sorted_activities = sorted(self.activities, key=lambda x: x.get_key())
+        return [x.get_description() for x in sorted_activities]
+
+    def get_num_activities(self) -> int:
+        """Returns the number of Activities in this instance."""
+        return len(self.activities)
+
+    def get_random_activity(self) -> "Activity":
+        """Returns a random Activity."""
+        return random.choice(self.activities)
+
+
+class Activity:
+    """Activity is a single practice task.
+
+    An Activity is build block of deliberate practice. Each Activity
+    represents a single pratice task that will be executed, graded
+    and repeated over time until the user has reached their desired
+    level of skill.
+    """
+
+    def __init__(self, description: str):
+        """Creates an Activity with the given description.
+
+        At this time, an activity is fully represented by a single
+        string.
+        """
+        self.description = description
+
+    def get_description(self) -> str:
+        """Returns the description of this Activity.
+
+        This should be enough information for the user to know how to
+        practice it.
+        """
+        return self.description
+
+    def get_key(self) -> str:
+        """Returns the key for this Activity.
+
+        Currently the key is just the description, but in the future it
+        should be a unique identifier that maps to this activity and
+        this activity only.
+        """
+        return self.get_description()
---
+++ routine_test.py
@@ -0,0 +1,84 @@
+import pathlib
+
+import pytest
+
+import routine
+
+
+class TestActivities:
+    def test_no_activity_file(self, tmp_path: pathlib.Path) -> None:
+        activity_file = pathlib.Path(tmp_path, "no_file.txt")
+
+        with pytest.raises(routine.InvalidActivitiesFileError):
+            routine.Activities(activity_file)
+
+    def test_empty_activity_file(self, tmp_path: pathlib.Path) -> None:
+        activity_file = pathlib.Path(tmp_path, "activities.txt")
+
+        # Open the file to create an empty file.
+        with open(activity_file, "w", encoding="utf-8"):
+            pass
+
+        with pytest.raises(routine.InvalidActivitiesFileError):
+            routine.Activities(activity_file)
+
+    def test_activity_file_one_activity(self, tmp_path: pathlib.Path) -> None:
+        activity_file = pathlib.Path(tmp_path, "activities.txt")
+        with open(activity_file, "w", encoding="utf-8") as f:
+            f.write("activity")
+
+        activities = routine.Activities(activity_file)
+        assert activities.get_num_activities() == 1
+
+        assert activities.get_activity_descriptions() == ["activity"]
+
+    def test_activity_file_many_activity(self, tmp_path: pathlib.Path) -> None:
+        activity_file = pathlib.Path(tmp_path, "activities.txt")
+        with open(activity_file, "w", encoding="utf-8") as f:
+            f.write("activity with Whitespace\n")
+            f.write("activity_1\n")
+            f.write("activity_2\n")
+            f.write("activity_3\n")
+            f.write("activity_4\n")
+
+        activities = routine.Activities(activity_file)
+        assert activities.get_num_activities() == 5
+        assert activities.get_activity_descriptions() == [
+            "activity with Whitespace",
+            "activity_1",
+            "activity_2",
+            "activity_3",
+            "activity_4",
+        ]
+
+    def test_activity_file_getting_many_random_activities(
+        self, tmp_path: pathlib.Path
+    ) -> None:
+        activity_file = pathlib.Path(tmp_path, "activities.txt")
+        with open(activity_file, "w", encoding="utf-8") as f:
+            f.write("activity_1\n")
+            f.write("activity_2\n")
+            f.write("activity_3\n")
+
+        activities = routine.Activities(activity_file)
+        assert activities.get_num_activities() == 3
+        assert activities.get_activity_descriptions() == [
+            "activity_1",
+            "activity_2",
+            "activity_3",
+        ]
+
+        # Ensure we can call random activity many more time then the
+        # number of activities we have.
+        for _ in range(20):
+            assert activities.get_random_activity()
+
+
+def test_activity_get_description() -> None:
+    act = routine.Activity("content")
+    assert act.get_description() == "content"
+
+
+def test_activity_get_key() -> None:
+    act = routine.Activity("content")
+    assert act.get_key() == "content"
