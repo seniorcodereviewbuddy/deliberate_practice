315b38186eec05b5353554bebda2e0f1f6ce33e5
Chris <chris@seniorcodereviewbuddy.com>
2024-06-07 11:51:17 -0400

    Enable checks for D104, requiring doc strings for public packages (#25)
    With the work on Django starting, this check seems helpful now.

---
+++ deliberate_practice_project/__init__.py
@@ -0,0 +1,5 @@
+"""Package for the deliberate practice django project.
+
+This is the high level container that contains the settings and
+configuration for the whole project.
+"""
---
+++ test_utils/__init__.py
@@ -0,0 +1,1 @@
+"""Package for test utils used by deliberate practice."""
