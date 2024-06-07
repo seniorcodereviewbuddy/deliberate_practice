f152505a387ae6c283052af09eff5b66c751c4bd
Chris Sharp <chris@seniorcodereviewbuddy.com>
2024-06-07 12:01:17 -0400

    Add initial empty deliberate_practice_app

---
+++ deliberate_practice_app/__init__.py
@@ -0,0 +1,6 @@
+"""Package for the deliberate project django app.
+
+Currently it's the only app used by the Django project.
+If clear splits become visible in the future, it's likely
+this be be split and renamed.
+"""
---
+++ deliberate_practice_app/views/__init__.py
@@ -0,0 +1,1 @@
+"""Package for all the deliberate practice views."""
