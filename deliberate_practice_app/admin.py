b509dcf37979e506ca56896b350e5726dbbbe199
Chris <chris@seniorcodereviewbuddy.com>
2024-06-07 12:05:08 -0400

    Add initial empty deliberate_practice_app (#26)
    Since this app will just contain everything, we'll call it
    deliberate_practice_app for now.
    
    Setting up views and models as packages instead of modules so we can
    start with separate files instead of having everything in one file for
    each.

---
+++ deliberate_practice_app/admin.py
@@ -0,0 +1,5 @@
+"""Module of all Django App code related to the admin page.
+
+Any models that should appear on the admin page need
+to be registered here.
+"""
---
+++ deliberate_practice_app/apps.py
@@ -0,0 +1,14 @@
+"""Package declaring all the App specific settings."""
+
+from django.apps import AppConfig
+
+
+class DeliberatePracticeAppConfig(AppConfig):
+    """DeliberatePracticeAppConfig is the config object App settings.
+
+    All configuration values specific to this application should be
+    located here.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "deliberat_practice_app"
