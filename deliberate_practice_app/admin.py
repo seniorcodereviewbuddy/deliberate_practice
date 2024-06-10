69e674d4649d804abf024274d80f0b6121f9838e
Chris Sharp <chris@seniorcodereviewbuddy.com>
2024-06-10 11:06:54 -0400

    Add PracticeSet and Activity models

--- deliberate_practice_app/admin.py
+++ deliberate_practice_app/admin.py
@@ -3,3 +3,10 @@
 Any models that should appear on the admin page need
 to be registered here.
 """
+
+from django.contrib import admin
+
+from .models import results, routine
+
+admin.site.register(routine.Activity)
+admin.site.register(results.PracticeSet)
---
+++ deliberate_practice_app/models/routine.py
@@ -0,0 +1,15 @@
+"""Module of the classes related to a practice routine."""
+
+from django.db import models
+
+
+class Activity(models.Model):
+    """Activity is a single practice task.
+
+    An Activity is build block of deliberate practice. Each Activity
+    represents a single pratice task that will be executed, graded
+    and repeated over time until the user has reached their desired
+    level of skill.
+    """
+
+    description = models.CharField(max_length=200)
