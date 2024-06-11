6fd026d5067344b913fcdff1219cce3beee92401
Chris <chris@seniorcodereviewbuddy.com>
2024-06-11 14:48:54 -0400

    BREAKING Add PracticeSession (#37)
    This change is breaking because it deletes a previous migration. While
    this should generally be avoided, since there aren't any database yet it
    seems acceptable. This avoids having to write code to handle how
    non-existent databases handle old PracticeSet models with no Practice
    Session key.
    
    Also adds tests for all models.

--- deliberate_practice_app/models/results.py
+++ deliberate_practice_app/models/results.py
@@ -1,5 +1,7 @@
 """Module of the classes related to practice results."""
 
+import enum
+
 from django.db import models
 
 from . import routine
@@ -13,12 +15,52 @@ POSSIBLE_SCORES = [
 ]
 
 
+class PracticeSession(models.Model):
+    """PracticeSession is a collection of PracticeSet."""
+
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(null=True)
+
+    class State(enum.Enum):
+        """State of the Practice Session.
+
+        Active session can have new PracticeSets added.
+        """
+
+        ACTIVE = 0
+        FINISHED = 1
+
+    def __str__(self) -> str:
+        return f"PracticeSession({self.id=}, {self.start_time=}, {self.end_time=})"
+
+    def state(self) -> State:
+        """Returns the state of this PracticeSession.
+
+        Mainly used to determine if more PracticeSets can
+        be added or not.
+        """
+        if self.end_time is None:
+            return PracticeSession.State.ACTIVE
+        return PracticeSession.State.FINISHED
+
+
 class PracticeSet(models.Model):
     """PracticeSet is an an Activity that was practiced and scored."""
 
-    activity_id = models.ForeignKey(routine.Activity, on_delete=models.CASCADE)
+    # on_delete is set to CADCASE for practice session since practice
+    # set should have the same lifetime as the session they live in.
+    practice_session = models.ForeignKey(
+        PracticeSession, on_delete=models.CASCADE, related_name="practice_sets"
+    )
+    # on_delete is set to PROTECT for acitivity because once we've
+    # practice an activity, we need to keep that activity around or
+    # practice session would look weird with missing data.
+    activity = models.ForeignKey(routine.Activity, on_delete=models.PROTECT)
     score = models.PositiveSmallIntegerField(choices=POSSIBLE_SCORES)
-    practice_time = models.DateTimeField()
+    practice_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
-        return f"PracticeSet({self.activity_id=}, {self.score=}, {self.practice_time=})"
+        return (
+            f"PracticeSet({self.practice_session=!s}, "
+            f"{self.activity=!s}, {self.score=}, {self.practice_time=})"
+        )
--- deliberate_practice_app/models/routine.py
+++ deliberate_practice_app/models/routine.py
@@ -13,3 +13,6 @@ class Activity(models.Model):
     """
 
     description = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f"Activity({self.description=})"
